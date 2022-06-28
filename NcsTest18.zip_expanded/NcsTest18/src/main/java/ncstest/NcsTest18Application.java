a0e4bedb5b4300dc74468320f489d5eb046e0a43
HS <35682300+gustn0307@users.noreply.github.com>
2022-06-28 19:17:47 +0900

    Add files via upload

---
+++ NcsTest18.zip_expanded/NcsTest18/src/main/java/ncstest/NcsTest18Application.java
@@ -0,0 +1,13 @@
+package ncstest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NcsTest18Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NcsTest18Application.class, args);
+	}
+
+}
---
+++ NcsTest18.zip_expanded/NcsTest18/src/test/java/ncstest/NcsTest18ApplicationTests.java
@@ -0,0 +1,13 @@
+package ncstest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NcsTest18ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
