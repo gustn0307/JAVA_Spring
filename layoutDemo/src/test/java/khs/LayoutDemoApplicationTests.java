dfeba10dd0724b24e64789ba14dbd11aafce9b6d
HS <35682300+gustn0307@users.noreply.github.com>
2022-06-28 19:08:42 +0900

    Add files via upload

---
+++ layoutDemo/src/test/java/khs/LayoutDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package khs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LayoutDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
